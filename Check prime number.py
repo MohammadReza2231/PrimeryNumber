3743522329c3ea44c47ec41ae25724a4a57fae00
mohammadreza <mohammad.reza.313it@gmail.com>
2022-08-09 23:14:57 +0430

    primere.py

---
+++ Check prime number.py
@@ -0,0 +1,18 @@
+def add_aval(m):
+    prime = True
+    for i in range(2,int(m**0.5)+1):
+        if m % i == 0:
+            prime = False
+            break
+    return(prime)
+
+
+prime_number_cuont=0
+LastPrimeNum=0
+for i in range(1,1001):
+    if add_aval(i):
+        prime_number_cuont = prime_number_cuont+1
+        LastPrimeNum=i
+
+print(" last prime num = ", LastPrimeNum)
+print("prime num cun = ", prime_number_cuont)
